4c4d23ff41893d63c9e094dea8b7f6544eeef358
SolO <35969476+WeirdNaturally@users.noreply.github.com>
2019-01-05 16:47:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var express= require('express')
+var app = express()
+var {
+        getbyName,
+        getCharactersfor,
+        getStafffor,
+        getSimilarfor,
+        getallbyName
+    }=require('./lib/anime')
+var {getTop100}=require('./lib/topAnime')
+var {getSeasonal}= require('./lib/seasonal')
+var {getTopCharacters}= require('./lib/characterby')
+var {searchbyName}=require('./lib/search')
+
+module.exports={
+    getbyName,
+    getCharactersfor,
+    getStafffor,
+    getSimilarfor,
+    getallbyName,
+    getTop100,
+    getSeasonal,
+    getTopCharacters,
+    searchbyName
+}
+
+
+
