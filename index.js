d149405250714c8f2d00a2036f0da50338dfd82c
Ashutosh1104 <asksingh11@gmail.com>
2020-08-25 13:09:41 +0530

    minor error correction

--- index.js
+++ index.js
@@ -22,25 +22,8 @@ module.exports={
     getSeasonal,
     getTopCharacters,
     searchbyName,
-    makingDb,
 }
-app.get('/',function(req,res){
-    makingDb().then(promises=>{
-        Promise.all(promises).then( temp => {
-            result=[]
-            console.log('.......................')
-            for( let i = 0 ; i < temp.length ; i++){
-                for( let j = 0 ; j < temp[i].length ; j++){
-                    result.push(temp[i][j])
-                    if( i == temp.length-1 && j == temp[i].length-1 ){
-                        console.log(result.length)
-                        res.send(result);
-                    }
-                }
-            }
-        }).catch(err=> console.log(err))
-    }).catch(err=> console.log(err))
-})
+
 app.listen('8000',function(){
     console.log('server started on 8000');
 })
